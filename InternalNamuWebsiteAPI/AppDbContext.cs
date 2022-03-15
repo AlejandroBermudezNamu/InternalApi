838c3f93705d6a2ef74ef78979779756ba245812
DavidCj <37236892+DavidCJ31@users.noreply.github.com>
2022-03-15 12:27:59 -0600

    Conexión DB

---
+++ InternalNamuWebsiteAPI/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using InternalNamuWebsiteAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternalNamuWebsiteAPI.Context
+{
+    public class AppDbContext: DbContext
+    {
+
+            public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+            {
+
+            }
+            public DbSet<User> User { get; set; }
+
+    }
+}
