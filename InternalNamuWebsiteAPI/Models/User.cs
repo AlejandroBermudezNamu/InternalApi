f0e369e094b3a2c88001918dbdff3b4977cc5451
DavidCj <37236892+DavidCJ31@users.noreply.github.com>
2022-03-15 12:25:37 -0600

    Model User

---
+++ InternalNamuWebsiteAPI/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternalNamuWebsiteAPI.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public bool Status { get; set; }
+        public string Email { get; set; }
+        public int FDCredits { get; set; }
+    }
+}
